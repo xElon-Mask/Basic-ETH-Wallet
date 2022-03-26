7c57f0b6d135e4d0e1397a6f6d90fee643297302
xElon-Mask <xelonmask@protonmail.com>
2022-03-26 20:07:26 +0100

    add getTotalBalance  function

--- basic_wallet.sol
+++ basic_wallet.sol
@@ -11,6 +11,10 @@ contract BasicWallet {
 
     mapping(address => wallet) Wallets;
 
+    function getTotalBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
     receive() external payable {
         Wallets[msg.sender].balance += msg.value;
         Wallets[msg.sender].numPayments += 1;
