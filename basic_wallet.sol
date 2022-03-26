9761375f73730d64c2618ec0e3422148a894e308
xElon-Mask <xelonmask@protonmail.com>
2022-03-26 20:19:34 +0100

    add withdrawAllMoney function

--- basic_wallet.sol
+++ basic_wallet.sol
@@ -19,6 +19,12 @@ contract BasicWallet {
         return Wallets[msg.sender].balance;
     }
 
+    function withdrawAllMoney(address payable _to) public {
+        uint _amount = Wallets[msg.sender].balance;
+        Wallets[msg.sender].balance = 0;
+        _to.transfer(_amount);
+    }
+
     receive() external payable {
         Wallets[msg.sender].balance += msg.value;
         Wallets[msg.sender].numPayments += 1;
